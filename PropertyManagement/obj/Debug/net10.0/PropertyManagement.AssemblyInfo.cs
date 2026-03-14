9ca3c0022714d2ca30749ee9acdd7885c8d91834
dougiehansen <dougiehansen@icloud.com>
2026-03-14 11:39:29 +0000

    refining property and unit page

--- PropertyManagement/obj/Debug/net10.0/PropertyManagement.AssemblyInfo.cs
+++ PropertyManagement/obj/Debug/net10.0/PropertyManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PropertyManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e12050365602de53f6d8785f2b253aa8fb8fdf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+114d7bf4c6cb2fab307ca6f908656564694ca6d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PropertyManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PropertyManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
